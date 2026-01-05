ff7a4f0bcb9c033f25b84d7875dea0699c8521d6
Rupesh Shrestha <rupeshshrestha2537@gmail.com>
2026-01-05 19:43:59 +1100

    frontend: init

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <> 
+      <h1>Test</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
